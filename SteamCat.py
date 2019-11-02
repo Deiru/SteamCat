8f99c2b11bec53ba128393ffc09580a77e959cd6
Deiru <deiru@hotmail.co.uk>
2019-11-02 21:46:20 +0000

    Add files via upload

--- SteamCat.py
+++ SteamCat.py
@@ -6,5 +6,7 @@ parsed = re.findall("/app/(\\d*)", toParse)#Parses file for all AppIDs, stores a
 
 storedAppIDs = open("addToVDF.txt", "a")
 category = input("Enter a name for this category:")
+numberAssigned = str(len(parsed))
 for x in parsed:
-    storedAppIDs.writelines(["\""+x+"\"\n", "{\n", "\t\"tags\"\n", "\t{\n", "\t\t\"0\"\t\""+category+"\"\n", "\t}\n", "}\n"])#Formatting for VDF
+    storedAppIDs.writelines(["\t\t\t\t\""+x+"\"\n", "\t\t\t\t{\n", "\t\t\t\t\t\"tags\"\n", "\t\t\t\t\t{\n", "\t\t\t\t\t\t\"0\"\t\""+category+"\"\n", "\t\t\t\t\t}\n", "\t\t\t\t}\n"])#Formatting for VDF
+print("Assigned "+numberAssigned+ " items to "+category)
